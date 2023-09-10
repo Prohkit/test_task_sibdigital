b3ecb7c717107b13a38ecfcb5558ea38264c6374
Никита <nikproserg@gmail.com>
2023-09-10 13:44:06 +0800

    Initial commit

---
+++ src/main/java/com/example/test_task_sibdigital/TestTaskSibdigitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_task_sibdigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskSibdigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskSibdigitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_task_sibdigital/TestTaskSibdigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_task_sibdigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskSibdigitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
